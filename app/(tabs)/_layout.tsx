5ab502631d0da8535625800f811ec708f674bfb2
Evan <evan@ablegroup.sg>
2024-10-10 11:59:43 +0800

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,6 +5,8 @@ import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+
+
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
